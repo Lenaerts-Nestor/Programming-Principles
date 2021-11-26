b45eabc86fd876613d2dbfb208f367ae22f968fd
Nestor Lenaerts <nestor.lenaerts@gmail.com>
2021-11-27 00:12:17 +0100

    bijgewerkt

--- H7/Paswoord generator methode/Program.cs
+++ H7/Paswoord generator methode/Program.cs
@@ -5,13 +5,54 @@ namespace Paswoord_generator_methode
     
     class Program
     {
-
-       
-        
+        static double Deel(double getal1, double getal2)
+        {
+            double getal = getal1 / getal2;
+            return getal;
+        }
+        static double VermenigVuldig(double getal1, double getal2)
+        {
+            double getal = getal1 * getal2;
+            return getal;
+        }
+        static double TrekAf(double getal1, double getal2)
+        {
+            double getal = getal1 - getal2;
+            return getal;
+        }
+        static double TelOp(double getal1, double getal2)
+        {
+            double getal = getal1 + getal2;
+            return getal;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("rekenmachine");
+            Console.WriteLine("kies een van de volgende (1:som),(2:min),(3:maal),(4:deel)");
+            int antwoord = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("shrijf twee getalen");
+            int getal1 = Convert.ToInt32(Console.ReadLine());
+            int getal2 = Convert.ToInt32(Console.ReadLine());
+            switch (antwoord)
+            {
+                case 1:
+                    Console.WriteLine(TelOp(getal1,getal2));
+                    break;
+                case 2:
+                    Console.WriteLine(TrekAf(getal1, getal2));
+                    break;
+                case 3:
+                    Console.WriteLine(VermenigVuldig(getal1, getal2));
+                    break;
+                case 4:
+                    Console.WriteLine(Deel(getal1,getal2));
+                    break;
+                default:
+                    Console.WriteLine("ERROR, Herstat de programma");
+                    break;
+            }
 
-            
+
         }
     }
 }
--- H7/Rekenmachine/Program.cs
+++ H7/Rekenmachine/Program.cs
@@ -4,9 +4,53 @@ namespace Rekenmachine
 {
     class Program
     {
+        static string passwordGeneratorMethode(int lengte)
+        {
+            //ik heb de oefening juist gemaakt, maar der is een kleine probleempje:
+            //de lengte word eentje langer of kleiner (bv: lengte = 15, resultaat: 14 of 16)
+
+            //variabelen
+            Random Generatormethode = new Random();
+            int bepaaldelengte = lengte;
+            char letters;
+            char hoofdletters;
+            int cijfers;
+            string password = "";
+
+            //bewerking
+            for (int i = 0; i <= bepaaldelengte; i++)
+            {
+                //random bedragen/letters genereren
+                letters = Convert.ToChar(Generatormethode.Next(60,90));
+                hoofdletters = Convert.ToChar(Generatormethode.Next(97, 123));
+                cijfers = Generatormethode.Next(1, 99);
+
+
+                //bijvoegen
+                password += letters;
+                i++;
+                password += hoofdletters;
+                i++;
+                password += Convert.ToString(cijfers);
+                i++;
+
+            }
+
+            //return de password
+             return password ;
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //variabelen
+            int lengte = 0;
+            //pasword generator:
+            Console.WriteLine("welkom bij de password generator: ");
+            Console.WriteLine("shrijf de lengte van de password:");
+            lengte = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"het password is: {passwordGeneratorMethode(lengte)}");
+            
         }
     }
 }
