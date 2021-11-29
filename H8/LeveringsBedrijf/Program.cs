7f03400c8a4e6a44dae3371b80098dfc6f8b3bae
Nestor Lenaerts <nestor.lenaerts@gmail.com>
2021-11-29 10:56:43 +0100

    update

---
+++ H8/LeveringsBedrijf/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace LeveringsBedrijf
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            //variabelen + array invullen direct (zelf kiezen)
+           
+            int[]postcode = new int[10] {2000,2020,2030,2050,2060,2080,2100,2120,2200,2500 };
+            int[] prijsPerKilo = new int[10] {35,37,38,42,46,70,80,90,120,300 };
+            int gekozenlocatie = 0;
+            int index = 0;
+            int gewicht = 0;
+            bool gevonden = false;
+            //vraag welke postcode hij wilt 
+            Console.WriteLine("naar waar wil je het pakket sturen");
+            gekozenlocatie = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("geef gewicht van pakket");
+            gewicht = Convert.ToInt32(Console.Read());
+            //controleer gekozenlocatie in de array en doe hele oefening
+            for (int i = 0; i < postcode.Length; i++)
+            {
+                if (gekozenlocatie == postcode[i])
+                {
+                    index = i;
+                    gevonden = true;    
+                }
+                else
+                {
+                    gevonden = false;
+                }
+                
+            }
+
+            if (gevonden = true)
+            {
+                Console.Write($"Prijs om te verzenden: {gewicht * prijsPerKilo[index]}$ naar postcode: {postcode[index]} ");
+                Console.WriteLine("");
+
+            }
+            else
+            {
+                Console.WriteLine($"de locatie: {gevonden} leveren we niet");
+            }
+           
+            
+
+
+
+
+            
+
+
+
+        }
+    }
+}
---
+++ H8/pittiger 5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace pittiger_5
+{
+    class Program
+    {
+
+        //nog niet aff
+        static void indrukken(int[] getal, int aantalrijen, int aantalkollomen)
+        {
+            for (int i = 1; i < aantalkollomen; i++)
+            {
+                
+                Console.WriteLine($"{i}: {getal[i]}");
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //variabelen
+            int[] arrayGetallen = new int[] { 4, 5, 8, 7, 5, 2, 6, 8, 7, 1, 3, 2 };
+            
+            int aantalkollomen = 3;
+            int aantalrijen = 3;
+
+
+            //array 
+            for (int i = 0; i < aantalrijen; i++)
+            {
+                if (true)
+                {
+                    Console.WriteLine($"{i}: ");
+                }
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
