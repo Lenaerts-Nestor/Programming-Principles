dcfa921b84aa807dabf2dc45142842189255662f
Nestor Lenaerts <nestor.lenaerts@gmail.com>
2021-12-01 13:58:42 +0100

    update leveringsBedrijf

--- H8/LeveringsBedrijf/Program.cs
+++ H8/LeveringsBedrijf/Program.cs
@@ -9,9 +9,9 @@ namespace LeveringsBedrijf
         {
 
             //variabelen + array invullen direct (zelf kiezen)
-           
-            int[]postcode = new int[10] {2000,2020,2030,2050,2060,2080,2100,2120,2200,2500 };
-            int[] prijsPerKilo = new int[10] {35,37,38,42,46,70,80,90,120,300 };
+
+            int[] postcode = new int[10] { 2000, 2020, 2030, 2050, 2060, 2080, 2100, 2120, 2200, 2500 };
+            int[] prijsPerKilo = new int[10] { 35, 37, 38, 42, 46, 70, 80, 90, 120, 300 };
             int gekozenlocatie = 0;
             int index = 0;
             int gewicht = 0;
@@ -27,13 +27,13 @@ namespace LeveringsBedrijf
                 if (gekozenlocatie == postcode[i])
                 {
                     index = i;
-                    gevonden = true;    
+                    gevonden = true;
                 }
                 else
                 {
                     gevonden = false;
                 }
-                
+
             }
 
             if (gevonden = true)
@@ -46,13 +46,11 @@ namespace LeveringsBedrijf
             {
                 Console.WriteLine($"de locatie: {gevonden} leveren we niet");
             }
-           
-            
 
 
 
 
-            
+
 
 
 
